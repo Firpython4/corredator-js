4661924c33f48661b9d26fcae82758cc8cc7a33c
Santiago Alt <santiagofirpo4@gmail.com>
2021-09-17 21:08:26 -0300

    Installed react-router-dom and added Routing

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+
+const routes = [
+     {
+         path: "/",
+         isPathExact: true,
+         component: null,
+         accessProvider: () => true
+     },
+     {
+        path: "/login",
+        isPathExact: true,
+        component: null,
+        accessProvider: () => true
+     },
+    {
+        path: "/submit",
+        isPathExact: true,
+        component: null,
+        accessProvider: () => true
+    }
+]
+
+export default routes;
